92ccf84b23c8edfc14c91506e9150238099eb4e1
David Camp <Bpendragon@users.noreply.github.com>
2019-12-01 20:45:48 -0800

    Refactored the daily downloader and setup script to be much nicer overall.

--- src/dailySetup.ps1
+++ src/dailySetup.ps1
@@ -3,12 +3,21 @@ param (
     [int]$Year = 0,
     [int]$Day = 0
 )
-$now = [DateTime]::UtcNow #If it's midnight in London, it's midnight on the US East Coast
-if($Year -lt 2015 -or $Day -lt 1 -or $Day -gt 25) #Basic params. if out of spec just get the most recent
+$now = [DateTime]::UtcNow.AddHours(-5) #EST is UTC -5, this makes it universal for all users. 
+
+if($Year -lt 2015 -or $Day -lt 1 -or $Day -gt 25 -or $Year -gt $now.Year -or ($Year -eq $now.Year -and $Day -gt $now.Day)) #Basic params. if out of spec just get the most recent
 {
      $Year = $now.Year
      $Day = $now.Day
+     if($now.Month -lt 12) #run the script mid year and get the most recent december 25th as a fall back
+     {
+         $Year = $now.Year - 1
+         $Day = 25
+     }
 }
+
+
+
 
 $config = Get-Content (Join-Path $PSScriptRoot -ChildPath "config.json") | ConvertFrom-Json
 
@@ -17,6 +26,16 @@ $InputURI = "https://adventofcode.com/$Year/day/$Day/input"
 $CodePath1 = Join-Path $PSScriptRoot -ChildPath "$Year\code\day$("{0:00}" -f $Day).ps1" #Pads a leading 0 if needed
 $CodePath2 = Join-Path $PSScriptRoot -ChildPath "$Year\code\day$("{0:00}" -f $Day)-2.ps1"
 
+if(!(Test-Path (Join-Path $PSScriptRoot -ChildPath "$Year\input"))) 
+{
+    mkdir (Join-Path $PSScriptRoot -ChildPath "$Year\input")
+}
+
+if(!(Test-Path (Join-Path $PSScriptRoot -ChildPath "$Year\code")))
+{
+    mkdir (Join-Path $PSScriptRoot -ChildPath "$Year\code")
+}
+
 if (!(Test-Path $InputPath)) {
     $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
     $cookie = New-Object System.Net.Cookie
@@ -24,12 +43,12 @@ if (!(Test-Path $InputPath)) {
     $cookie.Value = $config.cookie #Edit this as needed.
     $cookie.Domain = "adventofcode.com"
     $session.Cookies.Add($cookie);
-    Invoke-WebRequest -Uri $InputURI -WebSession $session -TimeoutSec 900 -Method Get -OutFile $inputPath    
+    Invoke-WebRequest -Uri $InputURI -WebSession $session -TimeoutSec 900 -Method Get -OutFile $inputPath
 }
 
 #Repeating the JoinPath here so as not to expose file structure to the world in the rest of the repo
 $BasicLayout = @"
-`$data = Get-Content (Join-Path `$PSScriptRoot -ChildPath "$Year\input\day$("{0:00}" -f $Day).txt")
+`$data = Get-Content (Join-Path `$PSScriptRoot -ChildPath "..\input\day$("{0:00}" -f $Day).txt")
 
 `$timer = New-Object System.Diagnostics.Stopwatch
 `$timer.Start()
